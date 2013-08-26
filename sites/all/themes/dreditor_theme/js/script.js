b8dd951b2e17363b808e9083c52b3d92129ece6c
Mark Carver <mark.carver@me.com>
2013-08-25 21:35:51 -0500

    Better handling of install button

--- sites/all/themes/dreditor_theme/js/script.js
+++ sites/all/themes/dreditor_theme/js/script.js
@@ -1,23 +1,41 @@
 (function($){
-  
+
+  var buttonText;
+
   Drupal.behaviors.dreditorTheme = {
     attach: function (context, settings) {
-      var $installed = $('#dreditor-is-installed');
       var $button = $('#install-dreditor');
-      if ($installed.length) {
-        var $icon = $button.find('.icon-download').clone().removeClass('icon-download').addClass('icon-ok');
-        $button.removeClass('btn-primary').addClass('btn-success').text(' Dreditor is installed!');
-        $button.prepend($icon);
+      if (!buttonText) {
+        buttonText = $button.text();
       }
+      var $icon = $('<i class="icon-white"></i>');
+      var $installed = $('#dreditor-is-installed');
+      if ($installed.length) {
+        var $icon = $button.find('.icon').clone().removeClass('icon-download').addClass('icon-ok');
+        $button.removeClass('btn-primary').addClass('btn-success').text(' Dreditor installed. Click to reinstall or update to ' + buttonText);
+        $icon.addClass('icon-ok');
+      }
+      else {
+        $button.text(' Install ' + buttonText);
+        $icon.addClass('icon-download');
+      }
+      $button.prepend($icon);
       if (typeof chrome !== 'undefined') {
         $button
           .attr('href', '#')
           .bind('click', function (e){
-            chrome.webstore.install();
+            chrome.webstore.install('https://chrome.google.com/webstore/detail/dhdpoembhlojpmehepeadblhglloobao', function () {
+              $button.removeClass('btn-primary').addClass('btn-success').text(' Successfully installed ' + buttonText + '!');
+              $button.prepend($('<i class="icon-white icon-ok"></i>'));
+            },
+            function () {
+              $button.removeClass('btn-primary').addClass('btn-error').text(' Failed to install ' + buttonText + '!');
+              $button.prepend($('<i class="icon-white icon-warning-sign"></i>'));
+            });
             e.preventDefault();
           });
       }
     }
   };
-  
+
 })(jQuery);
